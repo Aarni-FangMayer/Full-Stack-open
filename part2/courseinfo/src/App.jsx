4113b5e5189880b07a544c248d5cd79d87da3bdc
Arthur <aarni.fangmayer@gmail.com>
2025-07-17 15:10:15 +0300

    feat: complete 2.1 with destructured props and clean list styling

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,59 @@
+const Course = ({course}) => {
+  return (
+    <>
+      <Header courseName = {course.name} />
+      <Content parts = {course.parts} />
+      {/* <Total parts = {course.parts} /> */}
+    </>
+  )
+}
+
+const Header = ({courseName}) => <h1>{courseName}</h1>
+
+const Content = ({parts}) => (
+      <ul style={{ listStyleType: 'none', paddingLeft: 0 }}>
+        {parts.map(part => <Part key={part.id} part={part} /> )}
+      </ul>
+)
+
+const Part = ({part}) => <li>{part.name}: {part.exercises}</li>
+   
+
+// const Total = (props) => {
+//   return (
+//     <>
+//       <b>Number of exercises: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</b>
+//     </>
+//   )
+// }
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ],
+  }
+  
+  return (
+    <>
+      <Course course={course} />
+    </>
+  )
+}
+export default App;
