624f1b428fe18df5f6ee386cb0a8fd9fb9e552be
Aarni-FangMayer <aarni.fangmayer@gmail.com>
2025-08-30 01:14:37 +0300

    refactor: refactor BlogForm to use local state and handleSubmit

--- part5/bloglist/bloglist_frontend/src/assets/components/BlogForm/BlogForm.jsx
+++ part5/bloglist/bloglist_frontend/src/assets/components/BlogForm/BlogForm.jsx
@@ -1,20 +1,69 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import { useState } from "react";
+import "../../../services/blogs";
 import "./blogForm.css";
 
-const BlogForm = ({ addBlog, formData, setFormData }) => {
+const BlogForm = ({ addBlog }) => {
+  const [author, setAuthor] = useState("");
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!author || !name || !url) {
+      alert("All fields need to be filled");
+      return;
+    }
+
+    const defaultReviews = Math.floor(Math.random() * 11);
+
+    const newBlog = {
+      author,
+      name,
+      url,
+      reviews: defaultReviews,
+      likes: 0,
+    };
+
+    addBlog(newBlog);
+
+    setAuthor("");
+    setName("");
+    setUrl("");
+  };
+
   return (
     <div className="formSection">
       <h2>Add new blog here</h2>
-      <form onSubmit={addBlog}>
+      <form onSubmit={handleSubmit}>
         <label>
-          Author: <input type="text" placeholder="who is the author?" value={formData.author} onChange={(e) => setFormData({...formData, author: e.target.value})} />
+          Author:{" "}
+          <input
+            type="text"
+            placeholder="who is the author?"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
         </label>
         <label>
-          Blog Name: <input type="text" placeholder="do blog have name?" value={formData.name} onChange={(e) => setFormData({...formData, name: e.target.value})} />
+          Blog Name:{" "}
+          <input
+            type="text"
+            placeholder="do blog have name?"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
         <label>
-          Link: <input type="text" placeholder="where you can read it?" value={formData.url} onChange={(e) => setFormData({...formData, url: e.target.value})} />
+          Link:{" "}
+          <input
+            type="text"
+            placeholder="where you can read it?"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
         </label>
         <button type="submit">save</button>
       </form>
