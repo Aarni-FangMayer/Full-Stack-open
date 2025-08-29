c3587a724fe6ff0939f3877c69df9901ef22899a
Aarni-FangMayer <aarni.fangmayer@gmail.com>
2025-08-29 16:00:06 +0300

    feat: add LoginForm component for user authentication

---
+++ part5/bloglist/bloglist_frontend/src/assets/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import './loginForm.css'
+
+const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }) => {
+  return (
+    <div className="formSection">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            Username 
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password 
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
