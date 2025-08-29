aeb51d2053e8e01ad2345c79a9984ec827427597
Aarni-FangMayer <aarni.fangmayer@gmail.com>
2025-08-30 02:11:16 +0300

    feat: add toggleable short/extended view for blogs in BlogInfo component

--- part5/bloglist/bloglist_frontend/src/assets/components/BlogList/BlogInfo.jsx
+++ part5/bloglist/bloglist_frontend/src/assets/components/BlogList/BlogInfo.jsx
@@ -1,17 +1,38 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useState } from "react";
 import "./blogInfo.css";
 
 const BlogInfo = ({ id, author, name, url, reviews, likes, addLike, deleteBlog }) => {
-  return (
-    <div className="blogInfo">
+  const [showMoreInfo, setShowMoreInfo] = useState(false);
+
+  const shortInfo = () => {
+    return (
+      <div className="blogInfo">
+        <h3 className="titleHide">{name}</h3>
+        <butto id="showMoreButton" onClick={() => setShowMoreInfo(true)}>Show more info</butto>
+      </div>
+    )
+  };
+
+  const fullInfo = () => {
+    return (
+      <div className="blogInfo">
       <h3>Author: {author} </h3>
       <p>Title: {name} </p>
       <a href={url} target="_blank" rel="noopener noreferrer">{url}</a>
       <p>Reviews: {reviews}</p>
-      <button onClick={() => addLike(id)}>Likes count {likes}</button>
+      <p>Likes: {likes}</p>
+      <button onClick={() => addLike(id)}>Like</button>
       <button id="deleteButton" onClick={() => deleteBlog(id)}>Delete</button>
+      <button id="hideInfoButton" onClick={()=> setShowMoreInfo(null)}>hide info</button>
     </div>
+    )
+  }
+  return (
+    <>
+      {showMoreInfo ? fullInfo() : shortInfo()}
+    </>
+    
   );
 };
 
